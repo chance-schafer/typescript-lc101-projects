03a668e19f70ea6d88344456b47f99698e95d167
Chance Schafer <chance.schafer.dev@gmail.com>
2022-08-15 20:55:51 -0500

    completed Rocket.ts and compiling

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -6,8 +6,8 @@ import { Astronaut } from "./Astronaut";
 export class Rocket implements Payload{
     name: string;
     totalCapacityKg: number;
-    cargoItems: [];
-    astronauts: [];
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
     massKg: number
 
     constructor(name: string, totalCapacityKg: number) {
@@ -15,11 +15,37 @@ export class Rocket implements Payload{
         this.totalCapacityKg = totalCapacityKg
     }
 
-    sumMass ( items: Payload[] ): number {
+    sumMass( items: Payload[] ): number {
         let sum: number = 0
         for(let i = 0; i < items.length; i++) {
-            sum += items[i].massKg
+            sum += items[i].massKg;
         } 
-        return sum
+        return sum;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true
+        } else {
+            return false
+        }
     }
 }
